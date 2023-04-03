8fbc3a71d5f789694c29b54551e535b8bd28106a
Ombre <krys1014@wp.pl>
2023-04-03 19:34:26 +0200

    Added config to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'black': '#151515',
+      'green': '#00B85E',
+      'yellow': '#FFD928',
+      'gray': '#898989',
+      'white': '#FFFFFF',
+      'line': '#272727',
+    },
+    fontFamily: {
+      sans: ['Urbanist', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
