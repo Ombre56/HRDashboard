8f1cbf5d3a43db35fce42ddaf275488a7388e651
Ombre <krys1014@wp.pl>
2023-04-18 19:30:05 +0200

    Added new section -> job advertisings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import JobAds from './components/JobAds/JobAds';
 
 function App() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -10,14 +11,20 @@ function App() {
         <Header isOpen={isOpen} setIsOpen={setIsOpen} />
       </header>
 
-      <main>
-        <div className='hidden md:block'>
-          <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <main className='flex'>
+        <div>
+          <div className='hidden md:block'>
+            <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+          </div>
+
+          <div className={`mobile-menu h-screen z-50 absolute transition-all ease-in-out duration-500 ${isOpen ? "left-0" : "left-[-100%]"}`}>
+            {isOpen && <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />}
+          </div>
         </div>
 
-        <div className={`mobile-menu h-screen z-50 absolute transition-all ease-in-out duration-500 ${isOpen ? "left-0" : "left-[-100%]"}`}>
-          {isOpen && <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />}
-        </div>
+        <section className='w-full pt-8 pl-8'>
+          <JobAds />
+        </section>
       </main>
     </>
   );
