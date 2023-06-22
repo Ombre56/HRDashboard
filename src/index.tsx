27db863df5fe906ed3b73f6aee9bca64e18f7304
Ombre <krys1014@wp.pl>
2023-06-22 15:28:25 +0200

    Merge calendar

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Calendar from './components/Calendar/Calendar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Router>
-      <Routes>
-        <Route path="/" Component={App} />
-        <Route path='calendar' Component={Calendar} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
